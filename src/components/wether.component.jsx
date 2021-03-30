c715e9b610d7599673203b7760e56d3d8ba03077
NeroN-95 <romaburlaka1995@gmail.com>
2021-03-30 22:07:10 +0300

    upload weather.component.jsx to project

--- src/components/wether.component.jsx
+++ src/components/wether.component.jsx
@@ -14,7 +14,7 @@ const Weather = (props) => {
 
                 {props.temp_celsius ? (<h1 className="py-2">{props.temp_celsius}&deg;</h1>) : null}
 
-                {/** show min/max temp **/}
+                /** show min/max temp **/
                 {minmaxTemp(props.temp_min, props.temp_max)}
                 <h4 className="py-3">{props.description}</h4>
             </div>
